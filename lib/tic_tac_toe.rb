2f8c9a237a6e4a6ee9028d61c75323c4730567db
Tony <tonyjlum@gmail.com>
2018-10-19 01:43:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,3 +35,17 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board_array)
+  counter = 0
+  board_array.each {|el| counter +=1 if el != " "}
+  counter
+end
+
+def current_player(board_array)
+  if turn_count(board_array).even?
+    "X"
+  else
+    "O"
+  end
+end
